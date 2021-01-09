4b7e355259a9609afd71ed198ea19f315bfc1899
sagun shrestha <shtsgn13@gmail.com>
2021-01-09 08:28:07 +0545

    final push

---
+++ client/src/components/header/body.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Canva from '../canva/canva'
+import Sidebar from '../sidebar'
+import Text from '../sidebar/text'
+class Bodys extends Component {
+    state = {}
+    render() {
+        return (<div>
+            <Sidebar />
+
+        </div>);
+    }
+}
+
+export default Bodys;
